f21f6341a88bd3731c61acdd7ca6dbc0e8e2e9f2
matthewscoresby <63676677+matthewscoresby@users.noreply.github.com>
2023-09-19 13:10:03 -0600

    Fixed the Add to Cart Button
    The issue with the Add to Cart Button was it was not an array which made the map function unable to run. I fixed the issue by making it an array.

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,20 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Creating an array to hold the carts items
+
+  // Getting whats already saved to local storage
+  var tempCart = getLocalStorage("so-cart");
+
+  // If there is nothing in local storage then make tempCart an array
+  if (tempCart === null){tempCart = [];}
+
+  // Add the new product to the array
+  tempCart.push(product)
+
+  // Set the Local Storage to the new array
+  setLocalStorage("so-cart", tempCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
