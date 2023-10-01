3ccfe909d9d621fa5de406d49d5d4984cb803d4b
Lucas Gomes <lucaslehi@byui.edu>
2023-09-30 23:15:35 -0600

    updates on branch

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,11 +1,13 @@
 import { getLocalStorage } from "./utils.mjs";
+import { setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-
   //OS - cart.html error handling
   let cartItems = getLocalStorage("so-cart");
-  
-  if (cartItems === null){cartItems = [];}
+
+  if (cartItems === null) {
+    cartItems = [];
+  }
 
   //const cartItems = getLocalStorage("so-cart");
   // Added this
@@ -15,6 +17,7 @@ function renderCartContents() {
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
+  <a href="#" class="cart-card__remove" data-id="${item.Id}">X</a>
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -32,4 +35,26 @@ function cartItemTemplate(item) {
   return newItem;
 }
 
+document
+  .querySelector(".product-list")
+  .addEventListener("click", function (event) {
+    if (event.target.classList.contains("cart-card__remove")) {
+      // Retrieve the item's unique identifier (data-id)
+      const itemId = event.target.getAttribute("data-id");
+
+      // Remove the item from the cartItems array
+      let cartItems = getLocalStorage("so-cart");
+      if (cartItems === null) {
+        cartItems = [];
+      }
+
+      cartItems = cartItems.filter((item) => item.Id !== itemId);
+
+      // Update the cart in localStorage
+      setLocalStorage("so-cart", cartItems);
+
+      // Re-render the cart contents
+      renderCartContents();
+    }
+  });
 renderCartContents();
