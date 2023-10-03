5e31c9212f5a7b70058a128b40c0212f69d101de
thesaisema <sem19001@byui.edu>
2023-10-03 11:09:23 -0600

    Merge pull request #3 from otaviovaleriano/MS--Team2--Dynamic-Product-Detail-Pages Added functions in javascript regarding the items in the cart.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,5 +1,43 @@
 import { getLocalStorage } from "./utils.mjs";
 
+// (SAI)Assuming you have calculated the total amount and want to show it
+// and the Checkout button:
+const cartFooter = document.querySelector('.cart-footer');
+cartFooter.classList.remove('hide');
+
+// (SAI) Function to check if there are items in the cart
+function checkCartItems() {
+  // Check if there are items in local storage
+  const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+
+  if (cartItems.length > 0) {
+    // There are items in the cart, show the cart footer
+    const cartFooter = document.querySelector('.cart-footer');
+    cartFooter.classList.remove('hide');
+
+    // Calculate the total amount
+    let totalAmount = 0;
+    for (const item of cartItems) {
+      totalAmount += item.price * item.quantity;
+    }
+
+    // Create HTML to display the total
+    const totalHTML = document.createElement('p');
+    totalHTML.textContent = `Total: $${totalAmount.toFixed(2)}`;
+    totalHTML.classList.add('cart-total');
+
+    // Insert the total HTML into the cart footer element
+    cartFooter.appendChild(totalHTML);
+  } else {
+    // There are no items in the cart, you can perform other actions
+    console.log('The cart is empty.');
+  }
+}
+
+// Call the function when the cart page loads
+window.addEventListener('load', checkCartItems);
+
+
 function renderCartContents() {
 
   //OS - cart.html error handling
