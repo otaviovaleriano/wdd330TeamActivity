06286e6818b0e4c0c80fc2c08a93131c3f8111c6
matthewscoresby <mks@scoresby.net>
2023-10-06 16:20:28 -0600

    Update

--- src/js/components/ProductList.svelte
+++ src/js/components/ProductList.svelte
@@ -1,1 +1,21 @@
-<p>My new product list componenent!</p>
+<script>
+    import ProductSummary from "./ProductSummary.svelte";
+    import { getData } from "../productData.mjs";
+    
+    // this is how we make a prop in svelte
+    export let category;
+    // if you are looking at this thinking that's strange to just stop with a promise
+    // you would be right.  This will make more sense in a bit...stay tuned.
+    let promise = getData(category);
+</script>
+
+<h2>Top Products: {category}</h2>
+{#await promise}
+  Loading
+{:then products}
+<ul class="product-list">
+{#each products as product}
+<li class="product-card"><ProductSummary {product} /></li>
+{/each}
+</ul> 
+{/await} 
--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,5 @@ import ProductList from "./components/ProductList.svelte"
 
 new ProductList({
     target: document.querySelector(".products"),
+    props: { category: "tents" },
 });
