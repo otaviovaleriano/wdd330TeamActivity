259d1b4fc0ecf54f52225190ba872af72974fb42
Lucas Gomes <lucaslehi@byui.edu>
2023-12-11 18:05:32 -0700

    finalizes sorting

--- src/js/components/ProductList.svelte
+++ src/js/components/ProductList.svelte
@@ -7,9 +7,7 @@
 
   let promise = getData(category);
   let products = [];
-
-  // State to track sorting criteria
-  let sortBy = "name";
+  let sortBy = localStorage.getItem("sortBy") || "name";
 
   // Function to sort products based on criteria
   function sortProducts() {
@@ -23,41 +21,49 @@
   // Fetch data and sort on component mount
   onMount(async () => {
     products = await promise;
+    
     sortProducts();
+   
   });
 
   // Function to handle sorting change
   function handleSortChange(event) {
     sortBy = event.target.value;
+    console.log(sortBy)
+    localStorage.setItem("sortBy", sortBy);
     sortProducts();
+    location.reload();
+
   }
-  console.log(handleSortChange);
 
   // Function to get random product recommendations
-function getRandomRecommendations(products, count) {
-  // Clone the array to avoid modifying the original
-  const clonedProducts = [...products];
+  function getRandomRecommendations(products, count) {
+    // Clone the array to avoid modifying the original
+    const clonedProducts = [...products];
 
-  // Shuffle the array randomly
-  for (let i = clonedProducts.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [clonedProducts[i], clonedProducts[j]] = [clonedProducts[j], clonedProducts[i]];
+    // Shuffle the array randomly
+    for (let i = clonedProducts.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [clonedProducts[i], clonedProducts[j]] = [clonedProducts[j], clonedProducts[i]];
+    }
+
+    // Take the first 'count' products as recommendations
+    return clonedProducts.slice(0, count);
   }
-
-  // Take the first 'count' products as recommendations
-  return clonedProducts.slice(0, count);
-}
 </script>
 
 <h2>Top Products: {category}</h2>
 
-<!-- Dropdown for sorting -->
-<label for="sort">Sort by:</label>
-<select id="sort" bind:value={sortBy} on:change={handleSortChange}>
+<!-- Dropdown menu for sorting -->
+<div>
+<label for="sortSelector">Sort By:</label>
+<select id="sortSelector" bind:value={sortBy} on:change={handleSortChange}>
   <option value="name">Name</option>
   <option value="FinalPrice">Price</option>
 </select>
+</div>
 
+<!-- Product list -->
 {#if products.length === 0}
   <p>Loading</p>
 {:else}
@@ -67,12 +73,12 @@ function getRandomRecommendations(products, count) {
     {/each}
   </ul>
 
-
-<!-- Recommendations Section -->
-<h3>Recommended Products</h3>
-<ul class="recommendations-list">
-  {#each getRandomRecommendations(products, 3) as recommendedProduct}
-  <li class="product-card"><ProductSummary product={recommendedProduct} /></li>
-  {/each}
-</ul>
+  <!-- Recommendations Section -->
+  <h3>Recommended Products</h3>
+  <ul class="recommendations-list">
+    {#each getRandomRecommendations(products, 3) as recommendedProduct}
+      <li class="product-card"><ProductSummary product={recommendedProduct} /></li>
+    {/each}
+  </ul>
 {/if}
+
